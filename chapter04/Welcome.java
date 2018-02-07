c165adbed7e3b90212a7dd4bbf2aa43ee6590c1d
momoka <454768497@qq.com>
2018-02-07 17:33:03 +0800

    2018.2.7发光的单身狗Java基础5

---
+++ chapter04/Welcome.java
@@ -0,0 +1,18 @@
+public class Welcome{
+	public static void main(String[] args){
+		System.out.println("大家好，我是沈宁啊");
+	}
+}
+class Man{
+	
+}
+class Car{
+	
+}
+class Computer{
+	
+}
+/*我是沈宁
+这是我的代码
+欢迎一起学习JAVA
+*/
