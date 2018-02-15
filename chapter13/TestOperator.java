f4419a2f5e10d50665267cd45529d09085a784c2
momoka <454768497@qq.com>
2018-02-15 15:10:26 +0800

    2018.2.15

--- chapter13/TestOperator.java
+++ chapter13/TestOperator.java
@@ -26,8 +26,29 @@ public class TestOperator{
 		System.out.println(c2);
 		System.out.println(d2);
 		
-		int a2=3
+		int a2=3;
 		a+=5; //a=a+5;
+		
+		boolean b3=true&false;
+		System.out.println(b3);
+		
+		String str="5";
+		int s=4;
+		System.out.println(4+"5");
+		
+		int a4=3;
+		int b4=5;
+		String str1="";
+		/*
+		if(a4<b4){
+			str1="a4<b4";
+		}else{
+			str1="a4>b4";
+		}
+		*/
+		str1=(a4<b4)?"a4<b4":"a4>=b4";
+		System.out.println(str1);
+		
 		
 		
 	}
