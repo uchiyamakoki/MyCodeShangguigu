b6acdff1d537233af6421fc88e90263ccda4a6b5
momoka <454768497@qq.com>
2018-02-18 18:05:15 +0800

    2018.2.18

---
+++ chapter22/mypro01/src/mypro01/Hello.java
@@ -0,0 +1,12 @@
+package mypro01;
+
+public class Hello {
+	public static void main(String[] args){
+		//debug模式
+		int a=32;
+		int b=a*3;
+		int c=b*4;
+		System.out.println(c);
+		System.out.println("大家好，欢迎大家加入我们的学习！");
+	}
+}
---
+++ chapter22/mypro01/src/mypro01/TestFor.java
@@ -0,0 +1,14 @@
+package mypro01;
+
+public class TestFor {
+	public static void main(String[] args) {
+		//初始化
+		for(int a=1;a<=100;a++){ //条件循环
+			System.out.println(a); //循环体
+			//迭代
+		}
+		System.out.println("While循环结束");
+	}
+
+
+}
