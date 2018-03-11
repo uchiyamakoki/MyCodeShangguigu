e4c59f9a49732dd4d0be955a8c0bc4d85e8e661a
momoka <454768497@qq.com>
2018-03-11 17:12:42 +0800

    2017.3.11

---
+++ chapter60~63/testwrappedclass/src/cn/momoka/test.java
@@ -0,0 +1,15 @@
+package cn.momoka;
+
+public class test {
+    public static void main(String[] args) {
+        Integer i=new Integer(1000);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.toHexString(i));
+        Integer i2=Integer.parseInt("234");
+        Integer i3=new Integer("333");
+        System.out.println(i2+10);
+        System.out.println(i3.intValue());
+
+        String str=234+"";//数字转字符串
+    }
+}
